e3515630493de002905fbb658c9116f5b0a66720
HuuDungg <huudung2004kt@gmail.com>
2024-06-24 16:14:20 +0700

    check url and email

--- index.php
+++ index.php
@@ -1,21 +1,38 @@
 <?php 
 if($_SERVER['REQUEST_METHOD'] == "POST"){
     $messageError = "";
-if( empty($_POST["yourName"])){
-    $messageError = "name is required";
+    $messageEmail = "";
+    $messageUrl = "";
+    if( empty($_POST["yourName"])){
+        $messageError = "name is required";
+    }
+    if(!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
+        $messageEmail = "email is nnot valid";
+    }
+    if(!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $_POST["myUrl"])){
+        $messageUrl = "url is not valid";
+    }
 }else{
     echo $_POST['yourName'];
     echo "<br>";
     echo $_POST['gender'];
     echo "<br>";
     echo $_POST['country'];
-}
+    echo "<br>";
+    echo $_POST['email'];
 }
 ?>
 
 <form action="index.php" method="post">
+    input url <input type="text" name="myUrl">
+    <span style="color: red;"><?php echo $messageUrl;  ?></span>
+    <br>
     input your name <input type="text" name="yourName">
     <span style="color: red;"><?php echo $messageError;  ?></span>
+    <br>
+
+    input your email <input type="text" name="email">
+    <span style="color: red;"><?php echo $messageEmail;  ?></span>
     <br>
     choose your gender 
     <input type="radio" value="male" name="gender"> male 
