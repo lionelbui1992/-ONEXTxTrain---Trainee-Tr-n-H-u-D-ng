cb1a4cd6b7d9c286d140953b608d0244ab4d04f7
HuuDungg <huudung2004kt@gmail.com>
2024-06-24 15:49:24 +0700

    form handling

--- index.php
+++ index.php
@@ -1,12 +1,24 @@
-<?php
-$str = "huudung huudung huudung huudung";
-$pattern = "/huudung/i";
-//kiem tra xem co ton tai trong chuoi khong
-echo preg_match($pattern, $str); 
-
-//kem tra xem ton taij bao nhieu lan
-echo preg_match_all($pattern, $str);
-
-//thay the chuoi khop bang mot chuoi khac
-echo preg_replace($pattern, "hihi", $str);
+<?php 
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+    echo $_POST['yourName'];
+    echo "<br>";
+    echo $_POST['gender'];
+    echo "<br>";
+    echo $_POST['country'];
+    
+}
 ?>
+
+<form action="index.php" method="post">
+    input your name <input type="text" name="yourName"> <br>
+    choose your gender 
+    <input type="radio" value="male" name="gender"> male
+    <input type="radio" value="famele" name="gender"> female <br>
+    choose your country 
+    <select name="country">
+        <option value="VN">Viet Nam</option>
+        <option value="JP">Nhat Ban</option>
+        <option value="KR">Han Quoc</option>
+    </select> <br>
+    <button>Send</button>
+</form>
