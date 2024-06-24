4da86fa1b89c4e267c9cb98a46f153effa10fbfe
HuuDungg <huudung2004kt@gmail.com>
2024-06-24 16:27:41 +0700

    include and required

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php 
-    echo "today is " . date("d-m-Y");
-    echo "today is " . date("d/m/Y");
-    echo "today is " . date("d*m*Y");
+//chi chen 1 lan
+include_once "testInclude.php";
+
+//chen dc nhieu lan
+include "testInclude.php";
+
+//chi chen 1 lan
+require_once "testInclude.php";
+
+//chen dc nhieu lan
+require "testInclude.php";
     
+
     ?>
