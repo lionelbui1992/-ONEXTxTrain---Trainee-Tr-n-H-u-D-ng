c7bd89df50eafc2857ff2ef0c583f82e23ce2572
HuuDungg <huudung2004kt@gmail.com>
2024-06-24 14:42:31 +0700

    constant in php

--- index.php
+++ index.php
@@ -1,20 +1,5 @@
 <?php
-echo pi() . "<br />";
-echo min(3,2,3,5,3,53,7,65,3,1,2).'<br />';
-echo max(3,2,3,5,3,53,7,65,3,1,2).'<br />';
+define("hang1", "tran huu duung");
 
-//tra ve tri tuyet doi
-
-echo abs(-32) ."<br />";
-echo abs(39) ."<br />";
-
-//ham random
-echo rand() ."<br>";
-
-//tra ve can bac 2
-
-echo sqrt(100) ."<br />";
-//lam tron sau dau phay
-echo round(1.93432) ."<br />";
-
+define("hang2", 323);
 ?>
