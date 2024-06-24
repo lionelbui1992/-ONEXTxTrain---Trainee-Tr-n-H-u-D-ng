2427f3ccde1777d2376d37a4c878409aa7eb643e
HuuDungg <huudung2004kt@gmail.com>
2024-06-24 14:59:40 +0700

    if else elseif in php

--- index.php
+++ index.php
@@ -1,46 +1,12 @@
 <?php
-$x = 2;
-$y = 4;
+$a = 9;
 
-//toan tu cong
-echo $x + $y;
-//toan tu tru
-echo $x - $y;
-//toan tu nhan
-echo $x * $y;
-//toan tu chia
-echo $x / $y;
-//chia lay du
-echo $x % $y;
-//luy thua( x mu y)
-echo $x ** $y;
+if($a > 0){
+    echo "a lon hon 0";
+}elseif($a = 0 ){
+    echo "a lon bang 0";
+}else{
+    echo "a nho hon 0";
+}
 
-//toan tu so sanh
-
-//so sanh bang
-$x == $y;
-
-//so sanh khac
-$x!= $y;
-
-//so sanh lon hon
-$x > $y;
-
-
-//so sanh nho hon
-$x < $y;
-
-//so sanh lon hon hoac bang
-$x >= $y;
-
-//so sanh nho hon hoac bang
-$x <= $y;
-
-//toan tu gan
-$x = $y;
-$x += $y;
-$x -= $y;
-$x *= $y;
-$x /= $y;
-$x %= $y;
 ?>
