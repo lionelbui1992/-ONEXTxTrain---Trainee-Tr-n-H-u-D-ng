6d8d60bac6419f1c6cdf1aefa3cbe3d1407939d1
HuuDungg <huudung2004kt@gmail.com>
2024-06-24 15:59:11 +0700

    validate in php

--- index.php
+++ index.php
@@ -1,18 +1,24 @@
 <?php 
 if($_SERVER['REQUEST_METHOD'] == "POST"){
+    $messageError = "";
+if($_POST["yourName"] == ""){
+    $messageError = "must be not null";
+}else{
     echo $_POST['yourName'];
     echo "<br>";
     echo $_POST['gender'];
     echo "<br>";
     echo $_POST['country'];
-    
+}
 }
 ?>
 
 <form action="index.php" method="post">
-    input your name <input type="text" name="yourName"> <br>
+    input your name <input type="text" name="yourName">
+    <span style="color: red;"><?php echo $messageError;  ?></span>
+    <br>
     choose your gender 
-    <input type="radio" value="male" name="gender"> male
+    <input type="radio" value="male" name="gender"> male 
     <input type="radio" value="famele" name="gender"> female <br>
     choose your country 
     <select name="country">
@@ -22,3 +28,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
     </select> <br>
     <button>Send</button>
 </form>
+
+<?php 
+
+?>
