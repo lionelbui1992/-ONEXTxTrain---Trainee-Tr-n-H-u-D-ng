4793ac65698b165043a7ab4631a4c42bbea1f035
HuuDungg <huudung2004kt@gmail.com>
2024-06-24 14:15:19 +0700

    data types in php

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 $soNguyen = 134;
+echo var_dump($soNguyen);
 $soThuc = 32.43;
+echo var_dump($soThuc);
 $chuoi = "tran huu dung";
+echo var_dump($chuoi);
 $luanLy = true;
+echo var_dump($luanLy);
 
-
-echo "day la so nguyen $soNguyen";
-printf("day la so thuc $soThuc");
 ?>
