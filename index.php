480ae6324903ac452ccf89561ab45c6c5c53f864
HuuDungg <huudung2004kt@gmail.com>
2024-06-24 14:50:08 +0700

    magic constant in php

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
-define("hang1", "tran huu duung");
+// __CLASS__ tra ve ten class
 
-define("hang2", 323);
+// __DIR__ tra ve ten duong date_interval_create_from_date_string
+
+// __FILE__ tra ve ten file
+
+// __FUNCTION__ tra ve ten ham
+
+// __METHOD__ tra ve ten phuong thuc
+
+// __NAMESPACE__ tra ve ten namespace
+
+// __TRAIT__ tra ve ten trait
+
+// __LINE__ tra ve dong hien tai
+
 ?>
