eff0a86b6206561c1c4f231bbebe15a4b0a90d7f
HuuDungg <huudung2004kt@gmail.com>
2024-06-25 13:34:24 +0700

    limit and offset

--- index.php
+++ index.php
@@ -13,25 +13,27 @@ if ($conn->connect_error) {
 }
 
 // Gán giá trị $id
-$id = 4;
-
+$limit = 4;
+$offset = 0;
 // Chuẩn bị câu lệnh SQL để cập nhật bảng 'account'
-$stmt = $conn->prepare("UPDATE account SET email = ? WHERE id = ?");
-$stmt->bind_param("ss", $email, $id);
-
-// Gán giá trị cho các tham số
-$email = "da dsfdk co email";
-
-// Thực thi câu lệnh
+$stmt = $conn->prepare("select * from account limit $limit offset $offset");
 $stmt->execute();
-
-// Kiểm tra và hiển thị kết quả
-if ($stmt->affected_rows > 0) {
-    echo "Updated account with ID: " . $id;
-} else {
-    echo "No account updated.";
+$resul = $stmt->get_result();
+while($row = $resul->fetch_assoc()){
+    echo $row['username'];
+    echo $row['password'];
+    echo $row['email'];
+    echo $row['phone'];
+    echo $row['address'];
+    echo $row['gender'];
+    echo $row['birthday'];
+    echo $row['role'];
+    echo $row['status'];
+    echo $row['created_at'];
+    echo $row['updated_at'];
+    echo $row['deleted_at'];
+    echo "<br>";
 }
-
 // Đóng statement và kết nối
 $stmt->close();
 $conn->close();
