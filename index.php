6ddbbf2ece196852b1379156fa912feedc0bc8d7
HuuDungg <huudung2004kt@gmail.com>
2024-06-25 14:57:49 +0700

    use xml dom

--- index.php
+++ index.php
@@ -1,58 +1,11 @@
-<!DOCTYPE html>
-<html>
-<body>
-
 <?php
-//Initialize the XML parser
-$parser=xml_parser_create();
 
-//Function to use at the start of an element
-function start($parser,$element_name,$element_attrs) {
-    switch($element_name) {
-        case "NOTE":
-        echo "-- Note --<br>";
-    break;
-        case "TO":
-        echo "To: ";
-    break;
-        case "FROM":
-        echo "From: ";
-    break;
-        case "HEADING":
-        echo "Heading: ";
-    break;
-        case "BODY":
-        echo "Message: ";
-    }
-}
+$xmlFile = "note.xml";
+$xmlDOM = new DOMDocument();
 
-//Function to use at the end of an element
-function stop($parser,$element_name) {
-    echo "<br>";
-}
+$xmlDOM->load($xmlFile);
 
-//Function to use when finding character data
-function char($parser,$data) {
-    echo $data;
-}
 
-//Specify element handler
-xml_set_element_handler($parser,"start","stop");
+print $xmlDOM->saveXML();
 
-//Specify data handler
-xml_set_character_data_handler($parser,"char");
-
-//Open XML file
-$fp=fopen("note.xml","r");
-
-//Read data
-while ($data=fread($fp,4096)) {
-    xml_parse($parser,$data,feof($fp));
-}
-
-//Free the XML parser
-xml_parser_free($parser);
 ?>
-
-</body>
-</html>
