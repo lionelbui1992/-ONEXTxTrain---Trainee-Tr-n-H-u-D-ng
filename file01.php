af157c9ad595bb52a2e7c14d99f3a3005817def5
HuuDungg <huudung2004kt@gmail.com>
2024-06-25 10:21:01 +0700

    static in php

--- file01.php
+++ file01.php
@@ -1,22 +1,13 @@
 <?php
-// Định nghĩa một trait
-trait CanRun {
-    public function run() {
-        echo "Đang chạy.<br>";
-    }
-}
 
-// Sử dụng trait trong một lớp
 class Animal {
-    use CanRun;
-
-    public function makeSound() {
+    const NAME = 'animal';
+    public static function makeSound() {
         echo "Âm thanh của động vật.<br>";
     }
 }
+echo Animal::NAME;
 
-// Tạo một đối tượng từ lớp Animal
-$animal = new Animal();
-$animal->makeSound(); // Output: Âm thanh của động vật.
-$animal->run(); // Output: Đang chạy.
+echo Animal::makeSound();
+
 ?>
