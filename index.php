cbf0be3bd38d73d816b2c6f1c96306cdb412885e
HuuDungg <huudung2004kt@gmail.com>
2024-06-25 15:30:26 +0700

    use xmlhttp

--- index.php
+++ index.php
@@ -1,11 +1,32 @@
-<?php
+<html>
+<head>
+<script>
+    
+function showHint(str) {
+  if (str.length == 0) { 
+    document.getElementById("txtHint").innerHTML = "";
+    return;
+  } else {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("txtHint").innerHTML = this.responseText;
+      }
+    }
+    xmlhttp.open("GET", "gethint.php?q="+str, true);
+    xmlhttp.send();
+  }
+}
+</script>
+</head>
+<body>
 
-$xmlFile = "note.xml";
-$xmlDOM = new DOMDocument();
+<p><b>Start typing a name in the input field below:</b></p>
+<form action="">
+  <label for="fname">First name:</label>
+  <input type="text" id="fname" name="fname" onkeyup="showHint(this.value)">
+</form> 
+<p>Suggestions: <span id="txtHint"></span></p>
 
-$xmlDOM->load($xmlFile);
-
-
-print $xmlDOM->saveXML();
-
-?>
+</body>
+</html>
