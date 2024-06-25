639855225b3a21d8249ca8e1af8b16a17ede8dfe
HuuDungg <huudung2004kt@gmail.com>
2024-06-25 10:34:26 +0700

    use namespace in php

--- file01.php
+++ file01.php
@@ -1,17 +1,13 @@
 <?php
 
-class Animal {
-    const NAME = 'animal';
-
-    public static $age = 324324;
-    public static function makeSound() {
-        echo "Âm thanh của động vật.<br>";
+namespace space01;
+class Dog{
     }
-}
-echo Animal::NAME;
 
-echo Animal::makeSound();
 
-echo Animal::$age;
+namespace space02;
+class Dog{
+    }
 
+  
 ?>
--- index.php
+++ index.php
@@ -1,23 +1,9 @@
-<!DOCTYPE html>
-<html>
-<body>
-
 <?php
+    include "file01.php";
+
+    use space01\Dog;
+
+    $puppy = new Dog();
+
 
 ?>
-
-<form action="file01.php" method="post" enctype="multipart/form-data">
-    name: <input type="text" name="name"><br>
-    email: <input type="text" name="email"><br>
-    phonenumber: <input type="text" name="phonenumber"><br>
-    position: <select name="possition">
-        <option value="intern">intern</option>
-        <option value="dev">dev</option>
-        <option value="tester">tester</option>
-    </select><br>
-    your cv: <input type="file" name="fileUpload"><br>
-    <button type="submit">Send</button>
-</form>
-
-</body>
-</html>
