728088071f235701af98c94e92f71dd6e5e93e86
HuuDungg <huudung2004kt@gmail.com>
2024-06-25 10:57:57 +0700

    connect to database

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-    
+$server = "localhost";
+$user = "root";
+$password = "";
+$database = "myDataWeb";
+
+$conn = new mysqli($server, $user, $password, $database);
+
+if($conn->connect()){
+    echo "Connected";
+}else{
+    echo "Not Connected";
+}
 ?>
